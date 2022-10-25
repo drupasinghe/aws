d985ff9a34c9cb988cadaa559f74fb1f0619be70
Tharindu Rupasinghe <tharindu.rupasinghe@aeturnum.com>
2022-10-25 13:12:51 +0530

    owner id defined

--- variable.tf
+++ variable.tf
@@ -1,11 +1,22 @@
 variable "VPC_CIDR_BLOCK" {         
     type        = string
-    default     = "10.50.0.0/16"
+    default     = "10.50.0.0/16"  //variables added 
 
 }
 
 variable "subnet_test_public_CIDR" {
     type        = string
     default     = "10.50.10.0/24"
+}
+
+variable "owner_id" {
+    type        = string
+    default     = "678397654268"
+}
+
+
+variable "subnet_console_public_CIDR" {
+    type        = string
+    default     = "10.20.10.0/24"
 }
 
